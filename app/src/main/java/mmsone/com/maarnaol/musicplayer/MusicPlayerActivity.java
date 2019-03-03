b550b2c432dd81a1248c176c43e465ab0156932d
alcsuti <alcsutia@gmail.com>
2019-03-03 19:30:10 +0100

    distortion fix

--- app/src/main/java/mmsone/com/maarnaol/musicplayer/MusicPlayerActivity.java
+++ app/src/main/java/mmsone/com/maarnaol/musicplayer/MusicPlayerActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 
 import mmsone.com.maarnaol.R;
 import mmsone.com.maarnaol.databinding.ActivityMusicPlayerBinding;
@@ -20,6 +21,9 @@ public class MusicPlayerActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_music_player);
         ObjectAnimator flip = ObjectAnimator.ofFloat(binding.logo, "rotationY", 90f, -90f);
         flip.setDuration(2800);
+//        flip.setInterpolator(new DecelerateInterpolator());
+        float scale = getApplicationContext().getResources().getDisplayMetrics().density;
+        binding.logo.setCameraDistance(8000 * scale);
         flip.setRepeatCount(Animation.INFINITE);
         flip.start();
     }
